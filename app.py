231c25c31ed990be11378bb9d81613903424d3f5
vinaya2442 <156417487+vinaya2442@users.noreply.github.com>
2024-01-17 11:08:36 +0000

    calcu

--- app.py
+++ app.py
@@ -1,33 +1,30 @@
-  from flask import flask, render_template,request
+from flask import Flask, render_template, request
 
-  app=flask(__name__)
+app = Flask(__name__)
 
-  @app.rount('/')
-  def hello():
+@app.route('/')
+def hello():
     return render_template('index.html')
 
-    @app.rount("/result",methods=['GET','POST'])
-    def hi():
-        output=none
-        if request.method=='POST':
-            num1=int(request.from['num1'])
-            num2=int(request.from['num2'])
-            operation=request.from['operation']
-            print("Received from data - num1:",num1,"num2:",num2,"operation:",operation)
-            output=cal(num1, num2, operation)
-            print("calculated output:",output)
-            return render_template('result.html',operation=operation, output=output)
+@app.route("/result", methods=['GET', 'POST'])
+def hi():
+    output = None
+    if request.method == 'POST':
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        operation = request.form['operation']
+        output = cal(num1, num2, operation)
+    return render_template('result.html', operation=operation, output=output)
 
-            def cal(num1,num2,operation):
-                if operation == 'addition':
-                    return num1 + num2
-                elif operation == 'subtraction':
-                    return num1 - num2
-                elif operation == 'multiplication':
-                    return num1 == num2
-                elif operation == 'division':
-                    return num1 / num2
+def cal(num1, num2, operation):
+    if operation == 'addition':
+        return num1 + num2
+    elif operation == 'subtraction':
+        return num1 - num2
+    elif operation == 'multiplication':
+        return num1 * num2
+    elif operation == 'division':
+        return num1 / num2
 
-  if ___name__ ==  '__main__':
-       app.run()
-                          
+if __name__ == '__main__':
+    app.run()
